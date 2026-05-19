57bee4f7553f457cbcca8eb9bd77927f5b29d48b
tri3yul <yuliantotri851@gmail.com>
2026-05-19 09:55:36 +0700

    doc: add es6 classes code example

--- React/code/pemula-react/src/main.jsx
+++ React/code/pemula-react/src/main.jsx
@@ -1,21 +1,5 @@
 import { createRoot } from 'react-dom/client'
 
-const myelement = (
-  <table>
-    <tr>
-      <th>Name</th>
-    </tr>
-    <tr>
-      <td>John</td>
-    </tr>
-    <tr>
-      <td>Elsa</td>
-    </tr>
-  </table>
-);
-
 createRoot(document.getElementById('root')).render(
-  // <h1>Hello React!</h1>
-  <p>Welcome!</p>
-  // myelement
+  
 )
