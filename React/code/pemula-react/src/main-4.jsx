a9dc546064d43c1cdf2ac021c4c606148d2befeb
tri3yul <yuliantotri851@gmail.com>
2026-05-19 09:33:31 +0700

    fix: update material 4 code

--- React/code/pemula-react/src/main-4.jsx
+++ React/code/pemula-react/src/main-4.jsx
@@ -14,7 +14,7 @@ const myelement = (
   </table>
 );
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('sandy')).render(
   // <h1>Hello React!</h1>
   <p>Welcome!</p>
   // myelement
